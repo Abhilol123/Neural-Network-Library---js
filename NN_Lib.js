af3120d7d5bf47981c607261417b884669f406e4
abhinav <chandruabhi1968@gmail.com>
2022-01-30 09:32:30 +0530

    simplification of user input for NN

--- NN_Lib.js
+++ NN_Lib.js
@@ -1,41 +1,96 @@
 const Matrix = require("./Matrix.js");
 
 class NN {
-	constructor(no_inp_nodes, no_ans_nodes, no_hidden_layers, no_hidden_nodes, activation, dactivation) {
+	constructor(layer_info, activation = 'sigmoid', learning_rate = 0.1) {
 		this.no_inp_nodes = no_inp_nodes;
 		this.no_ans_nodes = no_ans_nodes;
 		this.no_hidden_layers = no_hidden_layers;
 		this.no_hidden_nodes = no_hidden_nodes;
-
+		this.layer_info = layer_info;
 		this.layers;
 		this.weights;
 		this.bias;
 		this.forw_prop;
-
 		this.activation = activation;
 		this.dactivation = dactivation;
-
-		this.learning_rate;
+		this.learning_rate = learning_rate;
+		this.output_matrix;
+		this.activation_function = {
+			'sigmoid': (number) => {
+				let solution = number;
+				solution = (1 / (1 + Math.pow(2.71828, -number)));
+				return solution;
+			},
+			'linear': (number) => {
+				return number;
+			},
+			'tanh': (number) => {
+				let solution = (2 / (1 + Math.pow(2.71828, -2 * number))) - 1;
+				return solution;
+			},
+			'relu': (number) => {
+				let solution = number;
+				if (solution < 0) {
+					solution = 0;
+				}
+				return solution;
+			},
+			'elu': (number) => {
+				let solution = number;
+				let alpha = 1;
+				if (solution < 0) {
+					solution = alpha * (Math.pow(2.71828, number) - 1);
+				}
+				return solution;
+			},
+		};
+		this.deactivation_function = {
+			'sigmoid': (number) => {
+				let solution;
+				solution = number * (1 - number);
+				return solution;
+			},
+			'linear': (number) => {
+				return 1;
+			},
+			'tanh': (number) => {
+				let solution = 1 - Math.pow(tanh(number), 2);
+			},
+			'relu': (number) => {
+				let solution = 1;
+				if (number < 0) {
+					solution = 0;
+				}
+				return solution;
+			},
+			'elu': (number) => {
+				let solution = 1;
+				let alpha = 1;
+				if (number < 0) {
+					solution = elu(number) + alpha;
+				}
+				return solution;
+			}
+		}
 	}
 
 	initialise() {
-		let temp;
-
+		// Creating layers of the neural network
 		this.layers = [];
-		temp = new Matrix(this.no_inp_nodes, 1);
-		this.layers.push(temp);
-		for (let i = 0; i < this.no_hidden_layers; i++) {
-			temp = new Matrix(this.no_hidden_nodes[i], 1);
+		for (let i = 0; i < this.layer_info.length; i++) {
+			temp = new Matrix(this.layer_info[i], 1);
 			this.layers.push(temp);
 		}
 		temp = new Matrix(this.no_ans_nodes, 1);
 		this.layers.push(temp);
 
+		// Assigning random weights
 		this.weights = [];
 		for (let i = 0; i < this.layers.length - 1; i++) {
 			this.weights.push(new Matrix(this.layers[i + 1].rows, this.layers[i].rows));
 		}
 
+		// Assigning random bias
 		this.bias = [];
 		for (let i = 0; i < this.layers.length - 1; i++) {
 			this.bias.push(new Matrix(this.layers[i + 1].rows, 1));
@@ -47,16 +102,18 @@ class NN {
 		for (let i = 0; i < this.bias.length; i++) {
 			this.forw_prop.push(Matrix.multiply(this.weights[i], this.forw_prop[i]));
 			this.forw_prop[i + 1] = Matrix.add(this.forw_prop[i + 1], this.bias[i]);
-			this.forw_prop[i + 1].map(this.activation);
+			this.forw_prop[i + 1].map(this.activation_function[this.activation]);
 		}
-		return (this.forw_prop[this.forw_prop.length - 1]);
+		this.output_matrix = this.forw_prop[this.forw_prop.length - 1];
 	}
 
-	backPropagation(output_matrix, _learning_rate) {
-		this.learning_rate = _learning_rate;
+	backPropagation(learning_rate) {
+		if (learning_rate) {
+			this.learning_rate = learning_rate;
+		}
 
 		// Calculate the weighted errors
-		let weighted_errors = [Matrix.subtract(output_matrix, this.forw_prop[this.forw_prop.length - 1])];
+		let weighted_errors = [Matrix.subtract(this.output_matrix, this.forw_prop[this.forw_prop.length - 1])];
 		for (let i = 1; i < this.bias.length; i++) {
 			let j = this.bias.length - i;
 			weighted_errors.push(Matrix.multiply(Matrix.transpose(this.weights[j]), weighted_errors[i - 1]));
@@ -66,7 +123,7 @@ class NN {
 		let gradients = [];
 		for (let i = 1; i < this.forw_prop.length; i++) {
 			let j = this.forw_prop.length - i - 1;
-			gradients.push(Matrix.map(this.forw_prop[i], this.dactivation));
+			gradients.push(Matrix.map(this.forw_prop[i], this.deactivation_function[this.activation]));
 			gradients[i - 1].multiply(weighted_errors[j]);
 			gradients[i - 1].multiply(this.learning_rate);
 		}
@@ -93,11 +150,20 @@ class NN {
 
 	mutate() {
 		for (let i = 0; i < this.weights.length; i++) {
-			this.weights[i].map(this.randomise)
+			this.weights[i].map(this._randomise)
 		}
 		for (let i = 0; i < this.bias.length; i++) {
-			this.bias[i].map(this.randomise)
+			this.bias[i].map(this._randomise)
 		}
+	}
+
+	_randomise(number) {
+		let solution = number;
+		let rate = 0.1;
+		if (Math.random() < rate) {
+			solution = solution + (Math.random() * 2 - 1) * 0.1
+		}
+		return solution;
 	}
 
 	static load_trained_NN(loadNN) {
@@ -112,84 +178,11 @@ class NN {
 		return newNN;
 	}
 
-	static sigmoid(number) {
-		let solution = number;
-		solution = (1 / (1 + Math.pow(2.71828, -number)));
-		return solution;
+	static save_trained_NN() {
+		return (JSON.stringify(this));
 	}
+}
 
-	static dsigmoid(number) {
-		let solution;
-		solution = number * (1 - number);
-		return solution;
-	}
-
-	static linear(number) {
-		return number;
-	}
-
-	static dlinear(number) {
-		return 1;
-	}
-
-	static tanh(number) {
-		let solution = (2 / (1 + Math.pow(2.71828, -2 * number))) - 1;
-		return solution;
-	}
-
-	static dtanh(number) {
-		let solution = 1 - Math.pow(tanh(number), 2);
-	}
-
-	static relu(number) {
-		let solution = number;
-		if (solution < 0) {
-			solution = 0;
-		}
-		return solution;
-	}
-
-	static drelu(number) {
-		let solution = 1;
-		if (number < 0) {
-			solution = 0;
-		}
-		return solution;
-	}
-
-	static elu(number) {
-		let solution = number;
-		let alpha = 1;
-		if (solution < 0) {
-			solution = alpha * (Math.pow(2.71828, number) - 1);
-		}
-		return solution;
-	}
-
-	static delu(number) {
-		let solution = 1;
-		let alpha = 1;
-		if (number < 0) {
-			solution = elu(number) + alpha;
-		}
-		return solution;
-	}
-
-	static randomise(number) {
-		let solution = number;
-		let rate = 0.1;
-		if (Math.random() < rate) {
-			solution = solution + (Math.random() * 2 - 1) * 0.1
-		}
-		return solution;
-	}
-
-	randomise(number) {
-		let solution = number;
-		let rate = 0.1;
-		if (Math.random() < rate) {
-			solution = solution + (Math.random() * 2 - 1) * 0.1
-		}
-		return solution;
-	}
+if (typeof module !== 'undefined') {
+	module.exports = NN;
 }
